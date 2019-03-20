8ac9a06e93b7f5c9072adb371dfe8122b29808e8
Lars Gruis <99047036@mydavinci.nl>
2019-03-20 17:07:45 +0100

    OOP: Pokemons aangemaakt

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 require 'pokemon.php';
+
+$pikachu =  new Pokemon('Pietje', 'Lightning', '60', 'Electric punch = 50', 'Pika Punch = 20', 'Fire', 'Fighting');
+$charmeleon =  new Pokemon('Kees', 'Fire', '60', 'Head Butt = 10', 'Flare = 30', 'Water', 'Lightning');
